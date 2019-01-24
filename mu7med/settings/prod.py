49cc6dd7c6d3a7bf6517cea7decd8a894c4ddce5
Mohamed Youssef <muhamed-youssef@outlook.com>
2019-01-24 11:06:12 +0200

    debug true

--- mu7med/settings/prod.py
+++ mu7med/settings/prod.py
@@ -1,10 +1,10 @@
 from .base import *
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = False
-# ADMINS = (
-#     ('Mohamed Youssef', 'mu7med.youssef@gmail.com'),
-# )
-ALLOWED_HOSTS = ['*']
+DEBUG = True
+ADMINS = (
+    ('Mohamed Youssef', 'mu7med.youssef@gmail.com'),
+)
+ALLOWED_HOSTS = ['mu7med.herokuapp.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
